818c4fddd95c666c07c3ff571542a8b4eab171a8
Jamil <jfayad@dti.crosemont.qc.ca>
2024-03-15 23:20:46 -0400

    Ajouter le bouton de signup fonctionel et peut sauter entre le signup et le login form.

--- ProjetWeb2/src/main/webapp/javascript/javascript.js
+++ ProjetWeb2/src/main/webapp/javascript/javascript.js
@@ -1,14 +1,34 @@
 document.addEventListener('DOMContentLoaded', function () {
     var loginFormDisplay = document.getElementById('logInForm');
-    var btnLogin = document.getElementById('btnLogin');
+    var btnLogin = document.getElementsByClassName('btnLogin');
     var closeLoginForm = document.getElementById('closeLoginForm');
+    var signupFormDisplay = document.getElementById('signUpForm');
+    var btnSignUp = document.getElementsByClassName('btnSignup');
+    var closeSignupForm = document.getElementById('closeSignupForm');
     
-    btnLogin.addEventListener('click', function () {
+    function handleLoginClick() {
+        signupFormDisplay.style.display = 'none';
         loginFormDisplay.style.display = 'flex';
         setTimeout(function () {
             loginFormDisplay.classList.add('active-form');
         }, 10);
-    });
+    }
+    
+    function handleSignupClick() {
+        loginFormDisplay.style.display = 'none';
+        signupFormDisplay.style.display = 'flex';
+        setTimeout(function () {
+            signupFormDisplay.classList.add('active-form');
+        }, 10);
+    }
+    
+    btnLogin[0].addEventListener('click', handleLoginClick);
+    btnLogin[1].addEventListener('click', handleLoginClick);
+    
+    btnSignUp[0].addEventListener('click', handleSignupClick);
+    btnSignUp[1].addEventListener('click', handleSignupClick);
+    
+    
 
     closeLoginForm.addEventListener('click', function () {
         event.preventDefault();
@@ -19,4 +39,15 @@ document.addEventListener('DOMContentLoaded', function () {
             loginFormDisplay.style.display = 'none';
         }, 1500);
     });
+    
+    closeSignupForm.addEventListener('click', function () {
+        event.preventDefault();
+        
+        signupFormDisplay.classList.remove('active-form');
+
+        setTimeout(function () {
+            signupFormDisplay.style.display = 'none';
+        }, 1500);
+    });
+    
 });
